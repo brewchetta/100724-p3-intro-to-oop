f928a434de07ba3653a92251244be312c14bdb34
brewchetta <chett.tiller@gmail.com>
2024-11-18 09:36:38 -0500

    updates for 100724

--- index.py
+++ index.py
@@ -1,12 +1,4 @@
 from classes.robot import Robot
-from classes.human import Human
-
-# import Robot from "./classes/robot"
+# from classes.human import Human
 
 print('Ready to make some robots!')
-
-walle = Robot(name="WALL-E")
-
-eve = Robot(name="EV-E", wd40="100oz")
-
-# we stopped here with the -i
